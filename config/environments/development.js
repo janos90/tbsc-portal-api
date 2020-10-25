18599b62ea4a2f503e05877f0c114082135425db
Juan <thekingliveson@gmail.com>
2020-10-26 08:06:34 +1300

    fixed cors issue

--- config/environments/development.js
+++ config/environments/development.js
@@ -1,4 +1,26 @@
 export default {
+
+  server: {
+  cors: {
+    origin: '*',
+    enabled: true,
+
+    headers: [
+      'Accept',
+      'Content-Type'
+    ],
+
+    methods: [
+      'GET',
+      'POST',
+      'PATCH',
+      'DELETE',
+      'HEAD',
+      'OPTIONS'
+    ]
+  }
+},
+
   logging: {
     level: 'DEBUG',
     format: 'text',
