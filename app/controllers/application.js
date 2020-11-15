153f6ee618ae9a350f164a358ed87519250041fd
Juan <garcij07@myunitec.ac.nz>
2020-11-15 16:13:15 +1300

    removed more auth components

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,10 +1,6 @@
 import { Controller } from 'lux-framework';
-import OAuth2Server from 'app/middleware/oauth2';
 
 class ApplicationController extends Controller {
-  // beforeAction = [
-  //   OAuth2Server.authenticate
-  // ];
 }
 
 export default ApplicationController;
